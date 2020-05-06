b36dc39fabe47f01b58d8c8533edf759336cb719
Nazmul <grey000906@gmail.com>
2020-05-07 01:54:32 +0600

    deploy portfolio

---
+++ precache-manifest.53592f8bc0991abf5ea12c46b8ebd0e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e5e954c66cd84ad0f7",
+    "url": "/vue-crud/css/app.c659ddc5.css"
+  },
+  {
+    "revision": "f9612dd5835417295158",
+    "url": "/vue-crud/css/chunk-55a3aae4.967689fa.css"
+  },
+  {
+    "revision": "723281a1d1958fb18f25",
+    "url": "/vue-crud/css/chunk-7040172c.d8b9cf18.css"
+  },
+  {
+    "revision": "3598bd403449e61656b349cdf9f33179",
+    "url": "/vue-crud/index.html"
+  },
+  {
+    "revision": "31e5e954c66cd84ad0f7",
+    "url": "/vue-crud/js/app.c5894e55.js"
+  },
+  {
+    "revision": "f9612dd5835417295158",
+    "url": "/vue-crud/js/chunk-55a3aae4.9d8bdac9.js"
+  },
+  {
+    "revision": "723281a1d1958fb18f25",
+    "url": "/vue-crud/js/chunk-7040172c.25942ccb.js"
+  },
+  {
+    "revision": "0e3efc9b593e296b7bae",
+    "url": "/vue-crud/js/chunk-vendors.2e0c849c.js"
+  },
+  {
+    "revision": "9ce80888e95e8ab2eb95b34bdd3080ab",
+    "url": "/vue-crud/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-crud/robots.txt"
+  }
+]);
