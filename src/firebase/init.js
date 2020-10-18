10eb35e5d54f17aadb30e3d14af7a3e9db9a7d64
Nazmul <grey000906@gmail.com>
2020-10-18 22:13:10 +0600

    nothing

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -10,8 +10,9 @@ var firebaseConfig = {
   storageBucket: 'job-title-tags.appspot.com',
   messagingSenderId: '324531096092',
   appId: '1:324531096092:web:84fa0424eeb6bcee6f2dd9',
-  measurementId: 'G-ZW5Z0M31YR'
+  measurementId: 'G-ZW5Z0M31YR',
 };
+
 // Initialize Firebase
 const webJobTags = firebase.initializeApp(firebaseConfig);
 // webJobTags.firestore().settings({timestampsInSnapshots: true })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
 import Index from '../components/Index.vue';
 
 Vue.use(VueRouter);
